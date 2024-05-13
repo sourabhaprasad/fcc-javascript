b65eba8c1ee64c27c3773d690769469cf2ea2c77
Sourabha K H <sourabhaprasad04@gmail.com>
2024-05-13 23:26:00 +0530

    uploaded code

---
+++ practice-works/advanced-arra-methods.js
@@ -0,0 +1,27 @@
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
+const evenListMedian = getMean([
+  testArr2[testArr2.length / 2 - 1],
+  testArr2[testArr2.length / 2],
+]);
+console.log(evenListMedian);
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+};
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+};
